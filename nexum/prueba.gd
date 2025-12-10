459b7c1664cb5f1891f1ad151a1509745ea57720
Germán Álvarez Gavilán <gag04@correo.ugr.es>
2025-12-10 09:44:25 +0100

    Creadas pruebas de cámara

--- nexum/prueba.gd
+++ nexum/prueba.gd
@@ -3,8 +3,24 @@ extends Node3D
 @onready var player = $Player
 @onready var enemies = $Zombie
 
+@onready var cameras := [$Camera1, $Camera2]
+@onready var current_index := 0
 
+func switch_camera():
+	# Apagar la cámara actual
+	cameras[current_index].current = false
+	
+	# Avanzar al siguiente índice
+	current_index = (current_index + 1) % cameras.size()
+	
+	# Activar la nueva cámara
+	cameras[current_index].current = true
+	
 func _ready() -> void:
 	enemies.player_target = player
+	cameras[current_index].current = true
 	pass
 	
+func _input(event):
+	if event.is_action_pressed("camera"):
+		switch_camera()
