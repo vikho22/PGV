568f90998ffa0bb2a934d3dbfbd10d393e21c6a3
javiercg04 <javcalgo@gmail.es>
2026-01-02 19:37:54 +0100

    Cambios en el Tutorial

--- nexum/Mapa/mapa.gd
+++ nexum/Mapa/mapa.gd
@@ -1,10 +1,11 @@
 extends Node3D
 
 @onready var player = $Player
-#@onready var zombie = $Zombie
-@onready var spawner = $Spawner
+@onready var zombie = $Zombie
+@onready var demon = $Demon2
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-#	zombie.player_target = player
-	spawner.objetivo_jugador = player
+	zombie.player_target = player
+	demon.player_target = player
 	pass # Replace with function body.
--- nexum/Tutorial/tutorial.gd
+++ nexum/Tutorial/tutorial.gd
@@ -4,6 +4,7 @@ extends Node3D
 enum Steps {
 	MOVE,
 	JUMP,
+	WEAPONS,
 	SHIELD,
 	ATTACK,
 	OBJECTS,
@@ -13,15 +14,15 @@ enum Steps {
 var current_step = Steps.MOVE
 @onready var label = $CanvasLayer/PanelContainer/Label
 
+
 func _ready() -> void:
 	get_tree().paused = true # Congela todo (física, enemigos)
 	%Zombie.player_target = null
-	%Zombie.hide()
-	%Bottle.hide()
-	%Apple.hide()
-	label.text = "Bienvenido al entrenamiento. Pulsa ENTER para empezar."
+	await get_tree().process_frame
+	get_tree().call_group("Objetos_Tutorial","hide")
+	label.text = "Bienvenido al entrenamiento. Pulsa INTERACT para empezar."
 	await get_tree().create_timer(2.0).timeout
-	update_instruction()
+	#update_instruction()
 
 func update_instruction() -> void:
 	match current_step:
@@ -29,18 +30,24 @@ func update_instruction() -> void:
 			label.text = "Usa WASD para caminar."
 		Steps.JUMP:
 			label.text = "¡Bien! Ahora pulsa ESPACIO para saltar el obstáculo."
+		Steps.WEAPONS:
+			%PickupPistol.show()
+			label.text = "Podemos tener diferentes armas, para seleccionarla deberemos de darle a la E"
 		Steps.SHIELD:
 			%Bottle.show()
-			label.text = "Te iran apareciendo objetos, para que puedas aumentar tu nivel de escudo, acercate a ellos para poder obtenerlo."
+			label.text = "Al matar enemigos podrán aparecer, para que puedas aumentar tu nivel de escudo, acercate a ellos para poder obtenerlo."
 		Steps.ATTACK:
+			label.text = "Pueden aparecer varios tipos de enemigos"
 			%Zombie.show()
+			%Demon.show()
 			%Zombie.player_target = %Player
+			%Demon.player_target = %Player
 			label.text = "Pulsa CLICK IZQUIERDO para atacar al objetivo."
 		Steps.OBJECTS:
 			%Apple.show()
 			if %Player.current_health < 100:
 				%Zombie.hide()
-			label.text = "Te iran apareciendo objetos, para que puedas aumentar tu nivel de vida, acercate a ellos para poder obtenerlo."
+			label.text = "Al matar enemigos podrán aparecer, para que puedas aumentar tu nivel de vida, acercate a ellos para poder obtenerlo."
 		Steps.FINISHED:
 			label.text = "¡Tutorial completado!"
 			await get_tree().create_timer(2.0).timeout
@@ -53,7 +60,7 @@ func complete_step(step_completed: int) -> void:
 		update_instruction()
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("enter") and get_tree().paused:
+	if event.is_action_pressed("interact") and get_tree().paused:
 		get_tree().paused = false 
 		update_instruction()
 
@@ -62,6 +69,9 @@ func _input(event: InputEvent) -> void:
 	
 	if current_step == Steps.ATTACK and event.is_action_pressed("attack"):
 		complete_step(Steps.ATTACK)
+	
+	if current_step == Steps.WEAPONS and event.is_action_pressed("interact"):
+		complete_step(Steps.WEAPONS)
 
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
